b3f22ac40fbd95f2aab3a329de3931efbccea204
rajesh <rajesh.chauhan23102008@gmail.com>
2018-10-25 00:27:29 +0530

    more types example added including func and object types.

--- app.js
+++ app.js
@@ -1,1 +1,73 @@
-console.log('hello from typescript!!!');
+"use strict";
+//console.log('hello from typescript!!!');
+//Types Examples
+var myAge = 20;
+//myAge='rajesh';
+var myName = 'rajesh';
+//myName = 36;
+var hasHobbies = true;
+//hasHobbies = 2;
+var user;
+user = 36;
+user = 'rajesh';
+var hobbies = ["sports", "food"];
+//hobbies=["100"];
+console.log(hobbies);
+console.log(typeof hobbies);
+var userHobbies = ["sporst", 786, "food", 9211];
+console.log(userHobbies);
+console.log(typeof userHobbies);
+//Tuples
+var myAddress = ["Palam Colony", 53];
+console.log(myAddress);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 200] = "Green";
+    Color[Color["Blue"] = 200] = "Blue";
+})(Color || (Color = {}));
+var myColor = Color.Green;
+console.log(Color.Red);
+console.log(myColor);
+console.log(Color.Blue);
+function getName() {
+    return myName;
+}
+console.log(getName());
+function returnMyName() {
+    return console.log(myName);
+}
+// Arguments/Input Types.
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+console.log(multiply(4, 5));
+// Function Types.
+var myFunction;
+myFunction = getName;
+console.log(myFunction());
+myFunction = multiply;
+console.log(myFunction(3, 8));
+var myTypeFunction;
+myTypeFunction = multiply;
+console.log(myTypeFunction(6, 6));
+// Object Type.
+var simpleObjectType = {
+    name: "rajesh",
+    age: 34
+};
+console.log(simpleObjectType);
+// Complex Object Type.
+var complexObjectType = {
+    data: [10, 20, 30, 40],
+    output: function (isThisOk) {
+        return this.data;
+    }
+};
+console.log(complexObjectType);
+var user1 = {
+    data: [100, 200, 300],
+    output: function (isRequired) {
+        return this.data;
+    }
+};
--- app.ts
+++ app.ts
@@ -1,1 +1,104 @@
-console.log('hello from typescript!!!');
+//console.log('hello from typescript!!!');
+
+//Types Examples
+
+let myAge: number = 20;
+//myAge='rajesh';
+
+let myName: string = 'rajesh';
+//myName = 36;
+
+let hasHobbies: boolean = true;
+//hasHobbies = 2;
+
+let user: any;
+user = 36;
+user = 'rajesh';
+
+let hobbies = ["sports", "food"];
+//hobbies=["100"];
+console.log(hobbies);
+
+console.log(typeof hobbies);
+
+let userHobbies: any[] = ["sporst", 786, "food", 9211];
+console.log(userHobbies);
+console.log(typeof userHobbies);
+
+//Tuples
+let myAddress: [string, number] = ["Palam Colony", 53];
+console.log(myAddress);
+
+enum Color {
+    Red,
+    Green = 200,
+    Blue = 200
+}
+
+let myColor: Color = Color.Green;
+
+console.log(Color.Red);
+console.log(myColor);
+console.log(Color.Blue);
+
+function getName(): string {
+    return myName;
+}
+
+console.log(getName());
+
+function returnMyName(): void {
+    return console.log(myName);
+}
+
+// Arguments/Input Types.
+function multiply(value1: number, value2: number): number {
+    return value1 * value2;
+}
+
+console.log(multiply(4, 5));
+
+// Function Types.
+let myFunction;
+
+myFunction = getName;
+console.log(myFunction());
+
+myFunction = multiply;
+console.log(myFunction(3, 8));
+
+let myTypeFunction: (num1: number, num2: number) => number;
+myTypeFunction = multiply;
+console.log(myTypeFunction(6, 6));
+
+// Object Type.
+let simpleObjectType: { name: string, age: number } = {
+    name: "rajesh",
+    age: 34
+};
+
+
+console.log(simpleObjectType);
+
+// Complex Object Type.
+
+let complexObjectType: { data: number[], output: (all: boolean) => number[] } = {
+    data: [10, 20, 30, 40],
+    output: function (isThisOk: boolean) {
+        return this.data;
+    }
+};
+
+console.log(complexObjectType);
+
+// Create your own Custom Type.
+type myCustomType = { data: number[], output: (all: boolean) => number[] };
+
+let user1: myCustomType = {
+    data: [100, 200, 300],
+    output: function (isRequired: boolean) {
+        return this.data;
+    }
+};
+
+console.log(user1);
