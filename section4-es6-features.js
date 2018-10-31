c4c08b871820128323fb078414af2ca6185f32bf
rajesh <rajesh.chauhan23102008@gmail.com>
2018-10-31 20:07:06 +0530

    Arrow Functions and Default Parameters

--- section4-es6-features.js
+++ section4-es6-features.js
@@ -16,3 +16,26 @@ function blockScope() {
 }
 blockScope();
 console.log(letVariable);
+// ARROW FUNCTIONS.
+console.log('ARROW FUNCTIONS');
+var addition = function (number1, number2) { return number1 + number2; };
+console.log(addition(4, 8));
+var arrowAddition = function (number1, number2) { return number1 + number2; };
+console.log(arrowAddition(30, 3));
+var hello = function () { return console.log('hello world'); };
+//console.log(hello());
+hello();
+var helloFriend = function (friend) { return console.log('hello ' + friend); };
+//console.log(helloFriend('rohit'));
+helloFriend('rohit');
+console.log('DEFAULT PARAMETERS!!!');
+var defaultParamFunc = function (start) {
+    if (start === void 0) { start = 15; }
+    console.log(start);
+    while (start > 0) {
+        start--;
+    }
+    console.log('Done!!!', start);
+};
+defaultParamFunc();
+defaultParamFunc(30);
--- section4-es6-features.ts
+++ section4-es6-features.ts
@@ -20,3 +20,33 @@ function blockScope() {
 
 blockScope();
 console.log(letVariable);
+
+// ARROW FUNCTIONS.
+console.log('ARROW FUNCTIONS');
+
+const addition = function(number1: number, number2: number): number { return number1 + number2};
+console.log(addition(4,8));
+
+const arrowAddition = (number1: number, number2: number) : number => number1 + number2;
+console.log(arrowAddition(30,3));
+
+const hello = () => console.log('hello world');
+//console.log(hello());
+hello();
+
+const helloFriend = (friend: string) : void => console.log('hello ' + friend);
+//console.log(helloFriend('rohit'));
+helloFriend('rohit');
+
+console.log('DEFAULT PARAMETERS!!!');
+
+const defaultParamFunc = (start: number = 15): void => {
+    console.log(start);
+    while(start > 0){
+        start--;
+    }
+    console.log('Done!!!',start);
+}
+
+defaultParamFunc();
+defaultParamFunc(30);
